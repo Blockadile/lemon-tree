1de02214fc589c21257ee2b6ff1fa7ba1b99a958
Blockadile <blockadile07@gmail.com>
2025-09-24 22:49:12 -0700

    Ninth commit (broken)

--- src/main/java/net/blockadile/lemon/LemonTree.java
+++ src/main/java/net/blockadile/lemon/LemonTree.java
@@ -17,10 +17,6 @@ import org.slf4j.LoggerFactory;
 
 public class LemonTree implements ModInitializer {
 	public static final String MOD_ID = "lemon-tree";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
--- src/main/java/net/blockadile/lemon/datagen/ModItemTagProvider.java
+++ src/main/java/net/blockadile/lemon/datagen/ModItemTagProvider.java
@@ -4,9 +4,7 @@ import net.blockadile.lemon.block.ModBlocks;
 import net.blockadile.lemon.util.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
